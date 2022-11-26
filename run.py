dc85c61d86570bcbcf0b47c73ae3b5a53d7117f5
Neillcllghn <neillcllghn@outlook.com>
2022-11-26 11:31:52 +0000

    setting up username details

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+
+def input_details():
+    
+        print('Please enter your username.')
+        print('username must not contain numbers or/and special characters')
+        print('Must be no longer than 6 characters')
+        print("Example: Neocal etc.....\n")
+
+        username = input("Enter your username here:\n")
+        print(f"Your username is {username}")
+
+input_details()
