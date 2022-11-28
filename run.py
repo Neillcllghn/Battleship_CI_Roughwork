9408289cd6ffabe9166718e757b16d15f926b61c
Neillcllghn <neillcllghn@outlook.com>
2022-11-28 19:02:57 +0000

    Update username and board

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from random import randint
 
 scores = {"computer": 0, "player": 0}
+HIDDEN_BOARD = [[' ']*10 for x in range(10)]
+GUESS_PATTERN = [[' ']*10 for x in range(10)]
+letters_to_numbers={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7, 'I':8, 'J':9,}
 
 
 def input_details():
@@ -32,15 +35,27 @@ def validate_username(values):
     elif any(char.isnumeric() for char in values):
         print(f"the username {values} cannot be used, please don't use numbers.")
         return False
-    elif any(char.isalnum() for char in values):
+    elif not any(char.isalnum() for char in values):
         print(f"the username {values} cannot be used, please don't use non-alphabetic characters.")
         return False
 
     return True
 
+def board_print_out(board):
+    print('A B C D E F G H I J')
+    print('---------------------')
+    row_of_numbers = 1
+    for row in board:
+        print("%d|%s|" % (row_of_numbers, "|".join(row)))
+        row_of_numbers += 1
 
-
-
+def ships_creation(board):
+    for ship in range(7):
+        ship_row, ship_column = randint(0,9), randint(0,9)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0,9), randint(0,9)
+        board[ship_row][ship_column] == 'X'
+        
 
 def ship_location_choices():
     """
